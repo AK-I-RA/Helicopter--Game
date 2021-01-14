f2698143a8f718b951bc524e64cb3e4973894d23
Ankit <ankitrawat7895@gmail.com>
2021-01-13 16:08:29 -0800

    Add files

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -7,3 +7,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<dos.h>
+
+int j=1;
+int left=200,top=200,radx=30,rady=15;
+int py1,py2,py3,py4,ran;
+long score=0;
+char ch;
+int t,l;
+int px;
+void downheli();
+void upheli();
+void chk_pipe();
+void heli_dam();
+void bk_m(int level);
+void dance_pipe(int px,int py);
