65d2525e3af312bdea818b2485712250e483b6b4
Ankit <ankitrawat7895@gmail.com>
2021-01-16 00:31:46 -0800

    Add files

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -115,3 +115,20 @@ chk_pipe();
 
 d_score(++score);
 }
+
+
+void removepipe(int px,int py)
+{
+
+setfillstyle(1,BLACK);
+//up pipes
+bar(px,py,px+60,py+200);
+bar(px-15,py+200,px+75,py+220);
+
+
+//down pipes
+bar(px-40,py+400,px+20,getmaxy()-50);
+bar(px-55,py+380,px+35,py+400);
+
+
+}
